b8f06abf71ab0837bef27e4cf894f4a084254509
Patrick Morris-Suzuki <github@morris-suzuki.com>
2013-10-02 08:23:45 -0400

    commit

--- nodejs/web.js
+++ nodejs/web.js
@@ -8,5 +8,6 @@ var server = require('http').createServer(app)
 server.listen(process.env.PORT || 5000);
 
 app.get('/*', function(req, res, next) {
-  res.redirect("http://www.reuters.com/article/2012/11/27/us-valley-ageism-idUSBRE8AQ0JK20121127");
+//  res.redirect("http://www.reuters.com/article/2012/11/27/us-valley-ageism-idUSBRE8AQ0JK20121127");
+  res.redirect("back");
 });
